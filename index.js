0044faa534b9ab097d7cf1e2af80fb652d82d74c
Nikola Foltánová <foltanova@ders.cz>
2023-04-03 22:36:01 +0200

    doplneni newslwtteru

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// ukol 1
 let car = document.getElementById("car");
 document.addEventListener("keydown", function (event) {
     if (event.key == "ArrowRight") {
@@ -8,6 +9,7 @@ document.addEventListener("keydown", function (event) {
     }
 });
 
+// ukol 2
 const user = {
     email: "bartolomej.rozumbrada@gmail.com",
     password: "vimzenicnevim",
@@ -32,3 +34,25 @@ form.addEventListener('submit', (event) => {
         passwordInput.value = '';
     }
 });
+
+// ukol 3 - newsletter pokračování z lekce
+var form2 = document.querySelector('form')
+var border = document.querySelector('#subscribe')
+border.addEventListener("input", function() {
+    // použití metody trim() slouží k odstranění prázdných znaků na začátku a na konci aby nedošlo k tomu, že uživatel napíše několik mezer
+    // pokud indexOf() nenalezne zavináče, tak vrátí -1 a proto podmínka, že se přidá border pokud indexOf === -1
+    if (border.value.trim()==="" || border.value.indexOf("@") === -1) {
+        border.classList.add("border")
+    } else {
+        border.classList.remove("border")
+    }
+})
+
+const subscribe = (event) => {
+	event.preventDefault()
+	var input = document.querySelector('input')
+	var email = input.value
+	form2.textContent = `Děkujeme za váš zájem. Těšte se na novinky ze světa frontendu a UX na vaší adrese ${email}.`
+}
+
+form2.addEventListener('submit', subscribe)
